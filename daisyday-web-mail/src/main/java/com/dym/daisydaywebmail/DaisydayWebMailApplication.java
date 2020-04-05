71638460238cce5db63e44ab2af30bc513bd6cb6
Daisy_day <dymdoudou@126.com>
2020-04-05 11:18:50 +0800

    Initial commit

---
+++ daisyday-web-mail/src/main/java/com/dym/daisydaywebmail/DaisydayWebMailApplication.java
@@ -0,0 +1,13 @@
+package com.dym.daisydaywebmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaisydayWebMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaisydayWebMailApplication.class, args);
+    }
+
+}
---
+++ daisyday-web-mail/src/main/java/com/dym/daisydaywebmail/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.dym.daisydaywebmail.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloController {
+
+
+    public String hello(){
+        return "hello";
+    }
+}
